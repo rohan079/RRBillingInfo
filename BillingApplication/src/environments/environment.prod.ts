b300441f6a2d8b9052e393eaaf428061e4436cd0
rohansunny <anaparthirohan@gmail.com>
2026-05-15 13:16:35 +0530

    docker push

---
+++ BillingApplication/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  /** Same-origin in Docker (nginx proxies /api to the backend). */
+  apiBaseUrl: '',
+  appDisplayName: 'RR Silks Inventory Info',
+};
